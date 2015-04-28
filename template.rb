5813d1b1c3b896c9cb2ef13239f59fef22a620e3
Petros Papadopoulos <petros.papadopoulos@ams-ix.net>
2015-04-28 22:04:01 +0200

    ...getting opinionated

--- template.rb
+++ template.rb
@@ -36,6 +36,7 @@ apply File.join(RECIPE_PATH, "gitignore.rb")
 git_commit "Initial commit of empty Rails engine."
 
 run "echo '#{name} -global' >> .rbenv-gemsets"
+run "rbenv local 2.2.1"
 
 RECIPES.each do |recipe|
   apply File.join(RECIPE_PATH, "#{recipe}.rb")
