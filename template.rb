4adb6b5f72f4d93f21d6b142e47fe15600e17048
Petros Papadopoulos <petros.papadopoulos@ams-ix.net>
2015-04-28 22:01:58 +0200

    Constants always on the top

--- template.rb
+++ template.rb
@@ -1,4 +1,10 @@
 #! /usr/bin/env ruby
+
+GEMSPEC_FILE = File.join(destination_root, "#{name}.gemspec")
+GITIGNORE_FILE = File.join(destination_root, ".gitignore")
+RECIPE_PATH = File.join(File.dirname(rails_template), "recipes")
+RECIPES = %w{dummy_app rspec guard developer_gems}
+
 error = false
 
 unless try(:name) && File.exist?(File.join(destination_root, 'lib', name, 'engine.rb'))
@@ -30,11 +36,6 @@ apply File.join(RECIPE_PATH, "gitignore.rb")
 git_commit "Initial commit of empty Rails engine."
 
 run "echo '#{name} -global' >> .rbenv-gemsets"
-
-GEMSPEC_FILE = File.join(destination_root, "#{name}.gemspec")
-GITIGNORE_FILE = File.join(destination_root, ".gitignore")
-RECIPE_PATH = File.join(File.dirname(rails_template), "recipes")
-RECIPES = %w{dummy_app rspec guard developer_gems}
 
 RECIPES.each do |recipe|
   apply File.join(RECIPE_PATH, "#{recipe}.rb")
