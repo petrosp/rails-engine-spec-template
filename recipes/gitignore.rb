aff55db115b24fd4b71d0325026f57821859b098
Petros Papadopoulos <petros.papadopoulos@ams-ix.net>
2015-04-28 18:07:00 +0200

    Be neutral

--- recipes/gitignore.rb
+++ recipes/gitignore.rb
@@ -2,7 +2,11 @@ say "Ignore some more files, it is not personal..."
 
 append_to_file '.gitignore' do
 "vendor/bundle
-.idea/"
+.idea/
+.rbenv-gemsets
+.rvmrc
+.ruby-gemset
+.ruby-version"
 end
 
 bundle
